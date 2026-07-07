d8734332c2bedcd167f0cd0688aac0570d000f31
Dias Suleimenov <dias.suleimenov@nu.edu.kz>
2026-07-07 14:48:51 +0500

    Fooling around with try-catch-finally and throwing

---
+++ week2/day4/src/Main.java
@@ -0,0 +1,23 @@
+class SpeedLimitExceeded extends RuntimeException {
+    public SpeedLimitExceeded(String message) {
+        super(message);
+    }
+}
+
+void main() {
+    try {
+        int[] arr = new int[67];
+//        arr[67]=667;
+        throw new SpeedLimitExceeded("You are running with 67 km/h!");
+
+    } catch (ArrayIndexOutOfBoundsException e){
+//    } catch (NullPointerException ex){
+        IO.println("67 >= 67 XD");
+    } catch (SpeedLimitExceeded e) {
+        IO.println("Damn they are running to fast" + e.toString());
+    }
+    finally{
+        IO.println("What finally?");
+    }
+    IO.println("GEGE is the mangaka");
+}
